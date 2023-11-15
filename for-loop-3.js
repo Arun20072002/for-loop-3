adb33a2bd12efc644f126c44484a57f197388502
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-11-15 09:50:15 +0530

    for-loop-3

---
+++ for-loop-3.js
@@ -0,0 +1,8 @@
+document.write("print a even number 1 to 100"+"<br>")
+document.write("-----------------------------"+"<br>")
+
+for(let a=1;a<=100;a++){
+    if(a%2==0){
+        document.write(a+"<br>")
+    }
+}
